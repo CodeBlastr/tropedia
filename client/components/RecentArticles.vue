d75bfc2494d747adad70b59876a2d681da830be5
Rafael Castillo <pcastill@mac.com>
2021-04-25 13:23:59 -0500

    Retrieve 5 most recent articles

---
+++ client/components/RecentArticles.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="my-3 p-3 bg-white rounded box-shadow">
+    <h6 class="border-bottom border-gray pb-2 mb-0">
+      Recent Articles
+    </h6>
+    <div v-for="a in articles" :key="a._id" class="media text-muted pt-3">
+      <img data-src="" alt="" class="mr-2 rounded">
+      <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+        <strong class="d-block text-gray-dark">{{ a.title }}</strong>
+        {{ a.description }}
+      </p>
+    </div>
+    <small class="d-block text-right mt-3">
+      <a href="#">All updates</a>
+    </small>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      articles: []
+    }
+  },
+  async fetch () {
+    this.articles = await this.$strapi.find('articles', { _limit: 5, _sort: 'published_at:desc' })
+  },
+  computed: {
+    isAuthenticated () {
+      return this.$strapi.user != null
+    },
+    user () {
+      return (this.isAuthenticated) ? this.$strapi.user : null
+    }
+  },
+  activated () {
+    if (this.$fetchState.timestamp <= Date.now() - 30000) {
+      this.$fetch()
+    }
+  }
+}
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,107 +1,14 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        Tropedia
-      </h1>
-      <div class="links">
-        <div v-if="!isAuthenticated">
-          <button
-            class="button--green"
-            @click="onRegisterClick"
-          >
-            Register
-          </button>
-          <button
-            class="button--grey"
-            @click="onSignInClick"
-          >
-            Sign In
-          </button>
-        </div>
-        <div v-if="isAuthenticated">
-          <p>
-            Welcome, {{ user.username }}
-          </p>
-          <button
-            class="button--red"
-            @click="onLogoutClick"
-          >
-            Logout
-          </button>
-        </div>
-      </div>
-    </div>
+  <div class="main-area">
+    <b-container>
+      <RecentArticles />
+    </b-container>
   </div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-
-    }
-  },
-  computed: {
-    isAuthenticated () {
-      return this.$strapi.user != null
-    },
-    user () {
-      return (this.isAuthenticated) ? this.$strapi.user : null
-    }
-  },
-  methods: {
-    onSignInClick () {
-      this.$router.push('/login')
-    },
-    onRegisterClick () {
-      this.$router.push('/register')
-    },
-    async onLogoutClick () {
-      await this.$strapi.logout()
-    }
-  }
-}
-</script>
-
 <style scoped>
-.container {
+.main-area {
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
