ca4eebb0211e791721aa2e7c79579ce220b54c52
Rafael Castillo <pcastill@mac.com>
2021-04-25 12:11:01 -0500

    Apply container to nav bar

--- client/components/TheNavBar.vue
+++ client/components/TheNavBar.vue
@@ -1,45 +1,47 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="dark">
-      <b-navbar-brand to="/">
-        Tropedia
-      </b-navbar-brand>
+      <b-container>
+        <b-navbar-brand to="/">
+          Tropedia
+        </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse" />
+        <b-navbar-toggle target="nav-collapse" />
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item href="#">
-            Link
-          </b-nav-item>
-          <b-nav-item href="#">
-            Link
-          </b-nav-item>
-        </b-navbar-nav>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">
+              Link
+            </b-nav-item>
+            <b-nav-item href="#">
+              Link
+            </b-nav-item>
+          </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search" />
-            <b-button size="sm" class="my-2 my-sm-0" type="submit" variant="outline-success">
-              Search
-            </b-button>
-          </b-nav-form>
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search" />
+              <b-button size="sm" class="my-2 my-sm-0" type="submit" variant="outline-success">
+                Search
+              </b-button>
+            </b-nav-form>
 
-          <b-nav-item-dropdown v-if="isAuthenticated" right class="ml-3">
-            <!-- Using 'button-content' slot -->
-            <template #button-content>
-              {{ user.username }}
-            </template>
-            <b-dropdown-item href="#" @click="onSignOutClick">
-              Sign Out
-            </b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item v-else href="#" to="/login">
-            Sign in
-          </b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
+            <b-nav-item-dropdown v-if="isAuthenticated" right class="ml-3">
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                {{ user.username }}
+              </template>
+              <b-dropdown-item href="#" @click="onSignOutClick">
+                Sign Out
+              </b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item v-else href="#" to="/login">
+              Sign in
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
     </b-navbar>
   </div>
 </template>
